598d1b714516a8d1ff55cf9a0c3366885d8c394a
Diego Toledo <toledo.diego09@gmail.com>
2017-04-16 15:00:09 -0500

    Initialize Graphx graph object from Cassandra data

--- build.sbt
+++ build.sbt
@@ -8,7 +8,8 @@ scalaVersion := "2.11.8"
 mainClass in Compile := Some("com.toledo.influencer_detector.App")
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.0.0",
-  "org.apache.spark" %% "spark-sql" % "2.0.0", 
+  "org.apache.spark" %% "spark-core" % "2.1.0",
+  "org.apache.spark" %% "spark-sql" % "2.1.0", 
+  "org.apache.spark" %% "spark-graphx" % "2.1.0", 
   "com.datastax.spark" %% "spark-cassandra-connector" % "2.0.0"
 )
--- src/main/scala/com/toledo/influencer_detector/App.scala
+++ src/main/scala/com/toledo/influencer_detector/App.scala
@@ -3,10 +3,19 @@ package com.toledo.influencer_detector
 import org.apache.spark.sql._
 import org.apache.spark._
 
+import org.apache.spark._
+import org.apache.spark.graphx.{ Graph, Edge, VertexId, VertexRDD }
+// To make some of the examples work we will also need RDD
+import org.apache.spark.rdd.RDD
+
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.cql._
 
 object App {
+
+  case class NodeRow(id: Long, name: String)
+  case class EdgeRow(source: Long, destination: Long)
+
   def main(args: Array[String]): Unit = {
 
     val keyspace = "influencer_detector"
@@ -14,15 +23,25 @@ object App {
     val edgesTable = "edges"
 
     val conf = new SparkConf(true)
+      .setAppName("Influencer Destector")
+      .setMaster("local[*]")
       .set("spark.cassandra.connection.host", "127.0.0.1")
 
-    val sc = new SparkContext("local[*]", "Influencer Destector", conf)
+    val sc = new SparkContext(conf)
 
-    val nodesRDD = sc.cassandraTable(keyspace, nodesTable)
-    val edgesRDD = sc.cassandraTable(keyspace, edgesTable)
+    // Retrieve graph data from Cassandra.
+    val nodeRows = sc.cassandraTable[NodeRow](keyspace, nodesTable).select("id", "name")
+    val edgesRows = sc.cassandraTable[EdgeRow](keyspace, edgesTable)
 
-    println("Number of nodes:" + nodesRDD.count())
-    println("Number of edges:" + edgesRDD.count())
+    // Initialize graphx
+    val nodes: RDD[(VertexId, (String))] =
+      nodeRows.map(nodeRow => (nodeRow.id, (nodeRow.name)))
 
+    val edges: RDD[Edge[Null]] = edgesRows.map(edgeRow => Edge(edgeRow.source, edgeRow.destination))
+    val defaultNode = ("None")
+
+    val graph: Graph[String, _] = Graph(nodes, edges, defaultNode)
+    graph.vertices.foreach(println)
+    graph.edges.foreach(println)
   }
 }
